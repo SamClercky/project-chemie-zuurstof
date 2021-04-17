d603ebef9d7264621a18e50d46b62375fa4960f1
SamClercky <sam.clercky@gmail.com>
2021-04-17 15:09:46 +0200

    Got rid of unsafe

--- backend/src/routes/gpio.rs
+++ backend/src/routes/gpio.rs
@@ -1,30 +1,27 @@
+use std::convert::Infallible;
 use tokio::sync::mpsc::Sender;
 use warp::{self, Filter};
 use warp::Rejection;
 use crate::gpio::GpioInstruction;
 
-static mut GPIO_TX: Option<Sender<GpioInstruction>> = None;
-fn get_gpio_tx() -> Option<Sender<GpioInstruction>> {
-    unsafe {
-        GPIO_TX.to_owned()
-    }
+fn with_gpio(tx: Sender<GpioInstruction>) 
+    -> impl Filter<Extract=(Sender<GpioInstruction>,), Error = Infallible> + Clone {
+    warp::any().map(move || tx.clone())
 }
 
-pub fn gpio(tx: Sender<GpioInstruction>) -> impl Filter<Extract=(warp::reply::Json,), Error=Rejection> + Clone {
-    unsafe {GPIO_TX = Some(tx);} // This is ok, because no other writes will happen
-
+pub fn gpio(tx: Sender<GpioInstruction>) 
+    -> impl Filter<Extract=(warp::reply::Json,), Error=Rejection> + Clone {
     return warp::post()
         .and(warp::path("gpio"))
         .and(warp::body::content_length_limit(1024*16))
         .and(warp::body::json())
-        .map(|instruction: GpioInstruction| {
+        .and(with_gpio(tx)) // send along tx
+        .map(|instruction: GpioInstruction, tx: Sender<GpioInstruction>| {
             let instr = instruction.clone();
             tokio::spawn(async move {
-                if let Some(tx) = get_gpio_tx() {
-                    tx.send(instr).await
-                        .map_err(|_| "Could not write to GPIO")
-                        .unwrap();
-                }
+                tx.send(instr).await
+                    .map_err(|_| "Could not write to GPIO")
+                    .unwrap();
             });
             warp::reply::json(&instruction)
         })

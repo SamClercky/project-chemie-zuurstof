1d81626aa2c0b0c8aca7c9a5c2466a33c50382d9
Sam Clercky <sam.clercky@gmail.com>
2021-04-24 21:06:47 +0100

    updated install and setup scripts

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-ln -s "$PWD/frontend/build" /tmp/www
+if [ -e /tmp/www ]; then
+	ln -s "$PWD/frontend/build" /tmp/www
+fi
+
+frontend_dir="$PWD/frontend"
+backend_dir="$PWD/backend"
+
+cd $frontend_dir && npm run build
+cd $backend_dir && RUST_LOG=debug cargo run

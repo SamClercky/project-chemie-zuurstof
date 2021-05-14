a01f915c35acc71feba7570a6d6024c9bd267517
Sam Clercky <sam.clercky@gmail.com>
2021-05-14 09:07:23 +0100

    Add release option

--- install.sh
+++ install.sh
@@ -8,4 +8,11 @@ frontend_dir="$PWD/frontend"
 backend_dir="$PWD/backend"
 
 cd $frontend_dir && npm run build
-cd $backend_dir && RUST_LOG=debug cargo run
+
+if [ -e "$backend_dir/target/release/backend" ]; then
+	echo "[*] Running release"
+	RUST_LOG=debug "$backend_dir/target/release/backend" 
+else
+	echo "[*] Running debug"
+	cd $backend_dir && RUST_LOG=debug cargo run
+fi
